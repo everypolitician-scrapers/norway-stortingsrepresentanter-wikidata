ffd5f8e15c8284796f12e00d9113fc5ca0a6a821
Tony Bowden <tony@tmtm.com>
2019-01-30 08:51:37 +0000

    Fetch in batches of 100
    This might save more memory

--- scraper.rb
+++ scraper.rb
@@ -6,4 +6,4 @@ require 'wikidata/fetcher'
 # has Property: "Storting ID"
 sparq = 'SELECT ?item WHERE { ?item wdt:P3072 [] }'
 ids = EveryPolitician::Wikidata.sparql(sparq)
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 250)
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 100)
